e52d212bb6ce955bed2bf8a8d46cf37973e87138
hassamk122 <hassamkiani66@gmail.com>
2025-07-22 12:55:05 +0500

    :)

--- src/pages/ShoppingCart.jsx
+++ src/pages/ShoppingCart.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 function ShoppingCart() {
   return (
-    <main className='min-h-screen'>ShoppingCart</main>
+    <main className='min-h-screen'>
+      <h1 className='text-center font-semibold text-lg  lg:text-3xl px-10 text-[#2f7eff]  lg:px-15 pt-5  lg:pt-10'>Shopping Cart</h1>
+      </main> 
   )
 }
 
